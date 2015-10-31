138d0d9665fb7c077ea6fcbd046134149352d467
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-31 15:47:41 +0100

    audio: Add LPA/Tunnel flags
    Change-Id: Ife6331f53a0633f6fd1b50b7afcc65974b323ec7

--- audio/include/system/audio.h
+++ audio/include/system/audio.h
@@ -814,6 +814,8 @@ typedef enum {
                                          // start voip over voice path.
     AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH = 0x1000, // flag for HDMI compressed passthrough
     AUDIO_OUTPUT_FLAG_DIRECT_PCM = 0x2000, // flag for Direct PCM
+    AUDIO_OUTPUT_FLAG_LPA = 0x8000,
+    AUDIO_OUTPUT_FLAG_TUNNEL = 0x10000
 } audio_output_flags_t;
 
 /* The audio input flags are analogous to audio output flags.
